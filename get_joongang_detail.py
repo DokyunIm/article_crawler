cb717a23882b52a1b9ca94652236e4be7bc563ff
Im Dokyun <imjoshua9316@gmail.com>
2018-06-26 22:17:03 +0900

    데이터 출력 코드 수정

--- get_joongang_detail.py
+++ get_joongang_detail.py
@@ -1,8 +1,6 @@
 import requests
-import json
 from bs4 import BeautifulSoup
 from konlpy.tag import Kkma
-from konlpy.utils import pprint
 
 def get(url):
     kkma = Kkma()
@@ -16,13 +14,39 @@ def get(url):
     header = req.headers
     status = req.status_code
     is_ok = req.ok
-    soup = BeautifulSoup(html, 'html.parser')
-    article_title = soup.select("#article_title")
-    print(article_title[0].text)
-    article_regdate = soup.select(".article_head > .clearfx > .byline > em")
-    print(article_regdate[1].text)
-    article_content = soup.select("#article_body")
-    #print(article_content[0].text)
-    word_list = list()
-    word_list = kkma.nouns(article_content[0].text)
-    return word_list
+
+    article_result = ""
+    result_sentence_list = list()
+    result_word_list = list()
+    print(status)
+    if(status == 200):
+        #print(html)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        article_title = soup.select("#article_title")
+        print(article_title[0].text)
+        article_regdate = soup.select(".article_head > .clearfx > .byline > em")
+        print(article_regdate[1].text)
+        article_content = soup.select("#article_body")
+        if(len(article_content[0].text)>0):
+            article_sentence_list = article_content[0].text.split(".")
+            word_list = ""
+
+            article_word_list = kkma.nouns(article_content[0].text)
+
+            for word in article_word_list:
+                if(len(word) >= 3):
+                    article_result = article_result+word+","
+                    result_word_list.append(word)
+            print(article_result)
+
+            for sentence in article_sentence_list:
+                if(len(sentence)>0):
+                    sentence_noun_list = kkma.nouns(sentence)
+                    for noun in sentence_noun_list:
+                        if(len(noun)>=3):
+                            word_list = word_list +noun+","
+                            result_sentence_list.append(word_list)
+                    word_list = ""
+            print(result_sentence_list)
+    return article_result, result_sentence_list, result_word_list
--- joongang_crawler.py
+++ joongang_crawler.py
@@ -1,15 +1,15 @@
 import requests
-import json
+import os
+import time
 from bs4 import BeautifulSoup
 import get_joongang_detail as get_detail
-import operator
 
 
 req_url = "http://search.joins.com/TotalNews"
 
 req_param = {
     'page': "1",
-    "Keyword": '감자',
+    "Keyword": "감자",
     "PeriodType": "DirectInput",
     #"StartSearchDate": "06/01/2018 00:00:00",
     #"EndSearchDate": "06/21/2018 00:00:00",
@@ -20,7 +20,10 @@ req_param = {
 }
 
 req_headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    'Upgrade-Insecure-Requests': '1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Cache-Control': 'max-age=0'
 }
 
 """
@@ -34,9 +37,17 @@ soup = BeautifulSoup(html, 'html.parser')
 article_link_list = soup.select(".headline > a")
 get_detail.get(article_link_list[0]['href'])
 """
-word_list = dict()
+current_dir = os.getcwd()
+if(os.path.isdir(current_dir+"/joongang") == False):
+    os.makedirs(current_dir+"/joongang")
+
+fp_result_sentence_csv = open(current_dir+"/joongang/"+req_param['Keyword']+"_sentence.csv", mode="w", encoding="utf-8")
+fp_result_article_csv = open(current_dir+"/joongang/"+req_param['Keyword']+"_article.csv", mode="w", encoding="utf-8")
+fp_result_freq_word_txt = open(current_dir+"/joongang/"+req_param['Keyword']+"_freq.txt", mode="w", encoding="utf-8")
+result_word_list = dict()
 idx = 0
-while(idx<=5):
+
+while(idx<=20):
     idx += 1
     req_param['page'] = str(idx)
     req = requests.get(req_url, headers=req_headers, params=req_param)
@@ -48,18 +59,50 @@ while(idx<=5):
     #article_link_list = soup.findAll("strong", {"class": "headline mg"})
     article_link_list = soup.select(".headline > a")
     if(len(article_link_list) == 0):
-        break;
+        break
     else:
         print(len(article_link_list))
         for link in article_link_list:
             print(link['href'])
-            article_word_list = get_detail.get(link['href'])
-            for word in article_word_list:
-                if(word not in word_list):
-                    word_list[word] = 1
+            article_list, sentence_list, word_list = get_detail.get(link['href'])
+            for word in word_list:
+                if (word not in result_word_list):
+                    result_word_list[word] = 1
                 else:
-                    word_list[word] += 1
+                    result_word_list[word] += 1
+            if(len(article_list) > 0):
+                fp_result_article_csv.write(article_list+"\n")
+                for sentence in sentence_list:
+                    if(len(sentence)!=0):
+                        fp_result_sentence_csv.write(sentence+"\n")
+            time.sleep(3.0)
 
+fp_result_freq_word_txt.write(str(result_word_list))
+
+fp_result_sentence_csv.close()
+fp_result_article_csv.close()
+fp_result_freq_word_txt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+for word in article_word_list:
+    if(word not in word_list):
+        word_list[word] = 1
+    else:
+        word_list[word] += 1
+"""
+
+"""
 sorted_word_list = sorted(word_list.items(), key=operator.itemgetter(1), reverse=True)
 
 fp = open(req_param['Keyword'], "w")
@@ -72,6 +115,6 @@ print(word_list)
 print(sorted_word_list)
 fp.close()
 fp_sorted.close()
-
+"""
 
 
