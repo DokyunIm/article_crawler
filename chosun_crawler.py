72e10e74cb7983ed641073b1d89bc2b31032ded0
Im Dokyun <imjoshua9316@gmail.com>
2018-06-26 22:14:08 +0900

    first commit

---
+++ chosun_crawler.py
@@ -0,0 +1,70 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+import time
+import get_chosun_detail as get_detail
+
+
+req_url = "http://search.chosun.com/search/news.search"
+
+req_param = {
+    "query": "감자",
+    "pageno": "0",
+    "orderby": "news",
+    "categoryname": "조선일보",
+    "c_scope": "navi",
+    "sdate": "2018.01.01",
+    "edate": "2018.06.25"
+}
+
+req_headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+}
+
+current_dir = os.getcwd()
+if(os.path.isdir(current_dir+"/chosun") == False):
+    os.makedirs(current_dir+"/chosun")
+
+fp_result_sentence_csv = open(current_dir+"/chosun/"+req_param['query']+"_sentence.csv", mode="w", encoding="utf-8")
+fp_result_article_csv = open(current_dir+"/chosun/"+req_param['query']+"_article.csv", mode="w", encoding="utf-8")
+fp_result_freq_word_txt = open(current_dir+"/chosun/"+req_param['query']+"_freq.txt", mode="w", encoding="utf-8")
+result_word_list = dict()
+idx = 0
+
+while(idx<=20):
+    idx += 1
+    req_param['pageno'] = str(idx)
+    req = requests.get(req_url, headers=req_headers, params=req_param)
+    html = req.text
+    header = req.headers
+    status = req.status_code
+    is_ok = req.ok
+    soup = BeautifulSoup(html, 'html.parser')
+    #article_link_list = soup.findAll("strong", {"class": "headline mg"})
+    article_link_list = soup.select(".search_news_box > dl > dt > a")
+    #body > div.schCont > div > div.l_area > div.search_news_box > dl: nth - child(2) > dt > a
+    if(len(article_link_list) == 0):
+        break
+    else:
+        print(len(article_link_list))
+        for link in article_link_list:
+            print(link['href'])
+            article_list, sentence_list, word_list = get_detail.get(link['href'])
+            for word in word_list:
+                if (word not in result_word_list):
+                    result_word_list[word] = 1
+                else:
+                    result_word_list[word] += 1
+            if(len(article_list) > 0):
+                fp_result_article_csv.write(article_list+"\n")
+                for sentence in sentence_list:
+                    if(len(sentence)!=0):
+                        fp_result_sentence_csv.write(sentence+"\n")
+            time.sleep(3.0)
+
+fp_result_freq_word_txt.write(str(result_word_list))
+
+fp_result_sentence_csv.close()
+fp_result_article_csv.close()
+fp_result_freq_word_txt.close()
---
+++ get_chosun_detail.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+from konlpy.tag import Kkma
+
+
+def get(url):
+    kkma = Kkma()
+    req_url = str(url)
+
+    req_headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    }
+    req = requests.get(req_url, headers=req_headers)
+    html = req.content
+    header = req.headers
+    status = req.status_code
+    is_ok = req.ok
+
+    article_result = ""
+    result_sentence_list = list()
+    result_word_list = list()
+    print(status)
+    if(status == 200):
+        #print(html)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        article_title = soup.select("#news_title_text_id")
+        print(article_title[0].text)
+        article_regdate = soup.select(".news_date")
+        print(article_regdate[0].text)
+        article_content = soup.select(".par")
+        #print(article_content[0].text)
+        if(len(article_content[0].text)>0):
+            article_sentence_list = article_content[0].text.split(".")
+            word_list = ""
+
+            article_word_list = kkma.nouns(article_content[0].text)
+
+            for word in article_word_list:
+                if(len(word) >= 3):
+                    article_result = article_result+word+","
+                    result_word_list.append(word)
+            print(article_result)
+
+            for sentence in article_sentence_list:
+                if(len(sentence)>0):
+                    sentence_noun_list = kkma.nouns(sentence)
+                    for noun in sentence_noun_list:
+                        if(len(noun)>=3):
+                            word_list = word_list +noun+","
+                            result_sentence_list.append(word_list)
+                    word_list = ""
+            print(result_sentence_list)
+    return article_result, result_sentence_list, result_word_list
