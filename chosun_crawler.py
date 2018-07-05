c29dd5948f2a1e8e02fbb0901e02a9b36978935a
Im Dokyun <imjoshua9316@gmail.com>
2018-07-05 16:50:29 +0900

    try-except 구조 추가

--- chosun_crawler.py
+++ chosun_crawler.py
@@ -6,81 +6,90 @@ import json
 from bs4 import BeautifulSoup
 import get_chosun_detail as get_detail
 
-req_url = "http://search.chosun.com/search/news.search" # 요청 URL
+try:
+    req_url = "http://search.chosun.com/search/news.search" # 요청 URL
 
-req_param = {
-    "query": "감자",
-    "pageno": "0",
-    "orderby": "news",
-    "categoryname": "조선일보",
-    "c_scope": "navi",
-    #"sdate": "2018.01.01",
-    #"edate": "2018.06.25"
-}
+    req_param = {
+        "query": "감자",
+        "pageno": "0",
+        "orderby": "news",
+        "categoryname": "조선일보",
+        "c_scope": "navi",
+        #"sdate": "2018.01.01",
+        #"edate": "2018.06.25"
+    }
 
-req_headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-    'Upgrade-Insecure-Requests': '1',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Cache-Control': 'max-age=0'
-}
+    req_headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+        'Upgrade-Insecure-Requests': '1',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Cache-Control': 'max-age=0'
+    }
 
-now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') # 현재시간
-current_dir = os.getcwd() # 현재 디렉토리 경로
-if(os.path.isdir(current_dir+"/chosun") == False): # 폴더 생성
-    os.makedirs(current_dir+"/chosun")
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') # 현재시간
+    current_dir = os.getcwd() # 현재 디렉토리 경로
+    if(os.path.isdir(current_dir+"/chosun") == False): # 폴더 생성
+        os.makedirs(current_dir+"/chosun")
 
-fp_word_sentence_csv = open(current_dir+"/chosun/"+req_param['query']+"_sentence_word("+str(now)+").csv", mode="w", encoding="utf-8") # 문장 단위 단어
-fp_word_article_csv = open(current_dir+"/chosun/"+req_param['query']+"_article("+str(now)+").csv", mode="w", encoding="utf-8") # 기사 단위 단어
-fp_freq_word_json = open(current_dir+"/chosun/"+req_param['query']+"_freq("+str(now)+").json", mode="w", encoding="utf-8") # 단어 빈도수
-fp_sentence_list_csv = open(current_dir+"/chosun/"+req_param['query']+"_sentenct_list("+str(now)+").csv", mode="w", encoding="utf-8") # 문자 리스트
+    fp_word_sentence_csv = open(current_dir+"/chosun/"+req_param['query']+"_sentence_word("+str(now)+").csv", mode="w", encoding="utf-8") # 문장 단위 단어
+    fp_word_article_csv = open(current_dir+"/chosun/"+req_param['query']+"_article("+str(now)+").csv", mode="w", encoding="utf-8") # 기사 단위 단어
+    fp_freq_word_json = open(current_dir+"/chosun/"+req_param['query']+"_freq("+str(now)+").json", mode="w", encoding="utf-8") # 단어 빈도수
+    fp_sentence_list_csv = open(current_dir+"/chosun/"+req_param['query']+"_sentenct_list("+str(now)+").csv", mode="w", encoding="utf-8") # 문자 리스트
 
-idx = 0 # 기사 리스트 인덱스
-word_freq = dict() # 단어 빈도수
+    idx = 0 # 기사 리스트 인덱스
+    word_freq = dict() # 단어 빈도수
 
-while(idx<=0):
-    idx += 1
-    req_param['page'] = str(idx)
-    req = requests.get(req_url, headers=req_headers, params=req_param)
-    html = req.text
-    header = req.headers
-    status = req.status_code
-    is_ok = req.ok
-    soup = BeautifulSoup(html, 'html.parser')
-    article_link_list = soup.select(".search_news_box > dl > dt > a")
+    while(idx<=50):
+        idx += 1
+        req_param['page'] = str(idx)
+        req = requests.get(req_url, headers=req_headers, params=req_param)
+        html = req.text
+        header = req.headers
+        status = req.status_code
+        is_ok = req.ok
+        soup = BeautifulSoup(html, 'html.parser')
+        article_link_list = soup.select(".search_news_box > dl > dt > a")
 
-    if(len(article_link_list) == 0):
-        break
-    else:
-        print("기사 수/페이지 : "+str(len(article_link_list)))
-        for link in article_link_list:
-            print(link['href'])
+        if(len(article_link_list) == 0):
+            break
+        else:
+            print("기사 수/페이지 : "+str(len(article_link_list)))
+            for link in article_link_list:
+                print(link['href'])
 
-            word_article_list, word_sentence_list, word_list, sentence_list = get_detail.get(link['href'])
+                word_article_list, word_sentence_list, word_list, sentence_list = get_detail.get(link['href'])
 
-            if(len(word_article_list)>0):
-                fp_word_article_csv.write(word_article_list+"\n")
+                if(len(word_article_list)>0):
+                    fp_word_article_csv.write(word_article_list+"\n")
 
-            if (len(word_sentence_list) > 0):
-                for words in word_sentence_list:
-                    fp_word_sentence_csv.write(words + "\n")
+                if (len(word_sentence_list) > 0):
+                    for words in word_sentence_list:
+                        fp_word_sentence_csv.write(words + "\n")
 
-            if(len(word_list)>0):
-                for word in word_list:
-                    if(word not in word_freq):
-                        word_freq[word] = 1
-                    else:
-                        word_freq[word] += 1
+                if(len(word_list)>0):
+                    for word in word_list:
+                        if(word not in word_freq):
+                            word_freq[word] = 1
+                        else:
+                            word_freq[word] += 1
 
-            if(len(sentence_list)>0):
-                for sentence in sentence_list:
-                    fp_sentence_list_csv.write(sentence+"\n")
+                if(len(sentence_list)>0):
+                    for sentence in sentence_list:
+                        fp_sentence_list_csv.write(sentence+"\n")
 
-            time.sleep(3.0)
+                time.sleep(3.0)
 
-fp_freq_word_json.write(json.dumps(word_freq, ensure_ascii=False))
+    fp_freq_word_json.write(json.dumps(word_freq, ensure_ascii=False))
 
-fp_freq_word_json.close()
-fp_sentence_list_csv.close()
-fp_word_sentence_csv.close()
-fp_word_article_csv.close()
+    fp_freq_word_json.close()
+    fp_sentence_list_csv.close()
+    fp_word_sentence_csv.close()
+    fp_word_article_csv.close()
+
+except:
+    fp_freq_word_json.write(json.dumps(word_freq, ensure_ascii=False))
+
+    fp_freq_word_json.close()
+    fp_sentence_list_csv.close()
+    fp_word_sentence_csv.close()
+    fp_word_article_csv.close()
